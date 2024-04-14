43171434550725fcacbd895f6d8a5e7a2a715111
Sushil <sushilpatil953825@gmail.com>
2024-04-14 14:25:41 +0530

    added Counter-3

--- React-1/PracticeReact/src/App.jsx
+++ React-1/PracticeReact/src/App.jsx
@@ -6,15 +6,22 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  function onclickHandler() {
-    setCount(count + 1);
-  }
-
   return (
     <div>
-      <button onClick={onclickHandler}>count={count}</button>
+      <Button count={count} setCount={setCount}></Button>
+      <Button count={count * 10} setCount={setCount}></Button>
+      <Button count={count * 100} setCount={setCount}></Button>
     </div>
   );
+}
+
+function Button(props) {
+  function onclickHandler() {
+    // eslint-disable-next-line react/prop-types
+    props.setCount(props.count + 1);
+  }
+  // eslint-disable-next-line react/prop-types
+  return <button onClick={onclickHandler}>Counter {props.count}</button>;
 }
 
 export default App;
